5eba388a5d29043ca1833d08ef26c1896f71161a
aplqo <aplqo@outlook.com>
2019-08-01 18:57:54 +0800

    Move display to lcd.c

--- include/lcd.h
+++ include/lcd.h
@@ -4,4 +4,9 @@
 unsigned char lcd_read();
 void lcd_write(unsigned char rs, unsigned char dat);
 
+/*---display--- */
+void display_time(unsigned char pos, unsigned int dat);
+void display_double(unsigned char pos, double dat);
+void display_str(unsigned char pos, unsigned char length, const char* str);
+
 #endif
--- src/lcd.c
+++ src/lcd.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+/*---lcd driver--- */
 unsigned char lcd_read()
 {
     unsigned char dat;
@@ -30,4 +31,54 @@ void lcd_write(unsigned char rs, unsigned char dat)
     asm("nop");
     asm("cbi PORTF,PORTF6");
     PORTB = 0xff;
+}
+/*---display---*/
+void display_time(unsigned char pos, unsigned int dat)
+{
+
+    lcd_write(0, 0x80 | pos);
+    unsigned char bit = 0;
+    unsigned int ratio = 36000, t = dat;
+    for (unsigned char i = 0; i < 6; i++)
+    {
+        bit = t / ratio;
+        lcd_write(1, bit + 0x30);
+        t = t % ratio;
+        if (t % 2)
+        {
+            ratio = ratio / 10;
+        }
+        else
+        {
+            ratio = ratio / 6;
+            lcd_write(1, ':');
+        }
+    }
+}
+void display_double(unsigned char pos, double dat)
+{
+    lcd_write(0, 0x80 | (pos + 8));
+    lcd_write(0, 0x04);
+    lcd_write(1, '%');
+    unsigned long j = (unsigned long)(dat * 1000000);
+    unsigned char bit = 0;
+    for (unsigned char i = 0; i < 6; i++)
+    {
+        bit = j % 10;
+        lcd_write(1, bit + 0x30);
+        if (i == 4)
+        {
+            lcd_write(1, '.');
+        }
+        j = j / 10;
+    }
+    lcd_write(0, 0x06);
+}
+void display_str(unsigned char pos, unsigned char length, const char* str)
+{
+    lcd_write(0, (0x80 | pos));
+    for (unsigned char i = 0; i < length; i++)
+    {
+        lcd_write(1, str[i]);
+    }
 }
